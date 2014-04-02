fe2d6525b7c9aefa2ed501f24e957240eaa050a7
Dan Cryer <dan@block8.co.uk>
2014-04-02 12:00:52 +0100

    Adding some more files to the skeleton project, mostly CSS/JS that was previously hosted elsewhere

---
+++ public/assets/js/ckeditor_styles.js
@@ -0,0 +1,4 @@
+CKEDITOR.stylesSet.add( 'styles', [
+//    { name: 'Intro', element: 'p', attributes: { 'class': 'intro' } },
+//    { name: 'Button', element: 'a', attributes: { 'class': 'btn btn-success' } }
+]);
